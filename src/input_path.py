7651d83b067e9fc88f2606b0d5dc4e6e0592d0cd
saeiddousti86 <36145015+saeiddousti86@users.noreply.github.com>
2020-03-19 08:23:12 -0400

    Update input_path.py

--- src/input_path.py
+++ src/input_path.py
@@ -6,5 +6,4 @@ def input_path_func():
     :return: the input file absolute path
     '''
     input_folder_path = os.path.abspath('../input')
-    input_content = os.listdir(input_folder_path)
-    return os.path.join(input_folder_path,input_content[0])
+    return os.path.join(input_folder_path,'complaints.csv')
