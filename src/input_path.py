f435a5b94c9bd2e06f027bc25e8947a3a5ee933c
saeid-dousti86 <61210011+saeid-dousti86@users.noreply.github.com>
2020-03-19 07:56:50 -0400

    Update input_path.py

--- src/input_path.py
+++ src/input_path.py
@@ -5,6 +5,6 @@ def input_path_func():
     This function retrieves the path of the input file from the repository
     :return: the input file absolute path
     '''
-    input_folder_path = os.path.abspath("../input")
+    input_folder_path = os.path.abspath('../input')
     input_content = os.listdir(input_folder_path)
     return os.path.join(input_folder_path,input_content[0])
